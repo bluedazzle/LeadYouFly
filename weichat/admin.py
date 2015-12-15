7c08f6756d15d1360d8623f0620650d9ca303a14
rapospectre <rapospectre@163.com>
2015-12-15 13:28:56 +0800

    update

--- weichat/admin.py
+++ weichat/admin.py
@@ -4,7 +4,8 @@ from weichat.models import WeChatAdmin, Promotion, Channel, Question
 # Register your models here.
 
 class PromotionAdmin(admin.ModelAdmin):
-    search_fields = ['nick']
+    list_display = ['nick', 'create_time']
+    search_fields = ['nick', 'sex']
     ordering = ('-create_time',)
 
 admin.site.register(WeChatAdmin)

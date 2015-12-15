4c62eedf58a1efc47b31625fa86a4224e2f1802a
rapospectre <rapospectre@163.com>
2015-12-15 13:24:45 +0800

    update

--- weichat/admin.py
+++ weichat/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from weichat.models import WeChatAdmin, Promotion, Channel, Question
 
 # Register your models here.
+
+class PromotionAdmin(admin.ModelAdmin):
+    search_fields = ['nick']
+    ordering = ('-create_time',)
+
 admin.site.register(WeChatAdmin)
-admin.site.register(Promotion)
+admin.site.register(Promotion, PromotionAdmin)
 admin.site.register(Channel)
 admin.site.register(Question)

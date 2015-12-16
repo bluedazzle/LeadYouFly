962a0fea99d655e9896ad5d3ca5fb4074b2597e3
rapospectre <rapospectre@163.com>
2015-12-16 17:00:38 +0800

    modify bigwheel

--- LYFSite/user_view.py
+++ LYFSite/user_view.py
@@ -291,8 +291,9 @@ def big_wheel(req):
     else:
         wx = WechatService()
         data = wx.get_user_info_by_code(code)
-        open_id = data.get('openid', None)
-        if open_id is None:
+        try:
+            open_id = data['openid']
+        except Exception, e:
             return HttpResponseRedirect('/luckyDraw')
         promotion_list = Promotion.objects.filter(open_id=open_id)
         if promotion_list.exists():

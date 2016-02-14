ff792d3b4648c4f8576eaf725ce2b81358d45a16
rapospectre <rapospectre@163.com>
2016-02-15 01:05:18 +0800

    add test

--- LYFSite/user_view.py
+++ LYFSite/user_view.py
@@ -269,8 +269,8 @@ def confirm_order(request):
                 wx = WechatService()
                 data = wx.get_user_info_by_code(code)
                 print data
-                open_id = data['openid']
-                union_id = data['unionid']
+                open_id = data.get('openid')
+                union_id = data.get('unionid')
                 student.wx_open_id = open_id
                 student.wx_union_id = union_id
                 student.save()

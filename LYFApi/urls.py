1b9e970d90e8a73545539cf0c83597f33441c7d3
rapospectre <rapospectre@163.com>
2016-03-12 13:58:33 +0800

    add a api interface:get_pending_orders

--- LYFApi/urls.py
+++ LYFApi/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, include, url
 from LYFApi import views
 
 urlpatterns = patterns('',
-                       url(r'^login$', views.login),)
+                       url(r'^login$', views.login),
+                       url(r'^pending_orders$', views.get_pending_orders),
+                       )
--- LYFApi/views.py
+++ LYFApi/views.py
@@ -5,11 +5,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-from LYFAdmin.models import Student
+from LYFAdmin.models import Student, Order
 # Create your views here.
 
 import json
 
+from LYFAdmin.utils import datetime_to_string
 from LeadYouFly.settings import SEO_HOST
 
 
@@ -39,3 +40,25 @@ def login(req):
             return HttpResponse(encodejson(2, body), content_type='application/json')
     body['message'] = '帐号不存在'
     return HttpResponse(encodejson(2, body), content_type='application/json')
+
+@csrf_exempt
+def get_pending_orders(req):
+    body = {}
+    token = req.POST.get('token')
+    if not token == 'rapospectreonly':
+        body['message'] = '接口访问权限不足'
+        return HttpResponse(encodejson(2, body), content_type='application/json')
+    order_list = Order.objects.filter(status=1)
+    result = []
+    if order_list.exists():
+        for order in order_list:
+            order_info = {}
+            order_info['student'] = '学员: {0}|{1}'.format(order.belong.account, order.belong.nick)
+            order_info['mentor'] = '教练: {0}|{1}'.format(order.teach_by.account, order.teach_by.nick)
+            order_info['info'] = '课程: {0}|价格: {1}|时间: {2}'.format(order.course_name, order.order_price, datetime_to_string(order.create_time))
+            result.append(order_info)
+        body['new'] = True
+    else:
+        body['new'] = False
+    body['order_list'] = result
+    return HttpResponse(encodejson(1, body), content_type='application/json')

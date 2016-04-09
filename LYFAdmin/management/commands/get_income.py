4ade9d28ea3cccc1ba5fc9e11758d7086ba8404f
rapospectre <rapospectre@163.com>
2016-04-09 10:47:31 +0800

    add get income

---
+++ LYFAdmin/management/commands/get_income.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+from LYFAdmin.models import Mentor
+import time
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        mentor = Mentor.objects.get(account='18744885070')
+        total = 0.0
+        ti = 0.0
+        for order in mentor.men_orders.all():
+            if order.mentor_money == 0.0:
+                total += order.order_price
+            else:
+                total += order.mentor_money
+                ti += order.platform_money
+        print '收入: {0}, 提成: {1}'.format(total, ti)

0f7807b69f92212a493b42ecddc7c494697a1be4
zhangjianqi <zhangjianqi@bytedance.com>
2018-04-17 01:44:26 +0800

    add
    Change-Id: I01baeccde31052fe045fc4ffb63dffcd6df19d8d

--- weichat/ctasks.py
+++ weichat/ctasks.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 import json
 import logging
+
+import datetime
 import requests
 
 from celery import Celery
@@ -34,11 +36,16 @@ def upload_picture(url, token, appid, secret):
 @app.task
 def gen_pic_and_send(nick, avatar, qr_url, openid, token, appid, secret, num):
     logging.info('Start notify url to baidu')
+    st = datetime.datetime(2018, 4, 22)
+    now = datetime.datetime.now()
+    day = unicode((st - now).days)
     MEDIA_TMP = './static/tmp/'
     # region = Image.open(cStringIO.StringIO(urllib.urlopen(qr_url).read()))
-    base_img = Image.open('{0}base00.png'.format(MEDIA_TMP))
+    # base_img = Image.open('bb.jpg')
+    base_img = Image.open('{0}bb.jpg'.format(MEDIA_TMP))
+    base_img = base_img.convert('RGBA')
     box = (150, 586, 240, 676)
-    ava_box = (212, 150, 312, 250)
+    ava_box = (220, 100, 320, 200)
     # box = (180, 632, 250, 702)
     # ava_box = (178, 500, 238, 560)
     # region.thumbnail((140, 140))
@@ -46,6 +53,8 @@ def gen_pic_and_send(nick, avatar, qr_url, openid, token, appid, secret, num):
     # region.thumbnail((90, 90))
     # base_img.paste(region, box)
     avatar = Image.open(cStringIO.StringIO(urllib.urlopen(avatar).read()))
+    avatar = avatar.convert('RGBA')
+    # avatar.show()
     # size = (120, 120)
     size = (100, 100)
     mask = Image.new('L', size, 0)
@@ -55,13 +64,18 @@ def gen_pic_and_send(nick, avatar, qr_url, openid, token, appid, secret, num):
     output.putalpha(mask)
 
     final1 = Image.new("RGBA", base_img.size)
-    final1.paste(base_img, (0, 0), base_img)
+    final1.paste(base_img, (0, 0))
     final1.paste(output, ava_box, output)
+    # final1.show()
     draw = ImageDraw.Draw(final1)
-    ttfont = ImageFont.truetype("{0}fzpc.ttf".format(MEDIA_TMP), 20)
-    draw.text((270, 285), nick, font=ttfont)
-    draw.text((295, 350), num, font=ttfont)
+    ttfont = ImageFont.truetype("{0}hr.ttf".format(MEDIA_TMP), 20)
+    ttfont1 = ImageFont.truetype("{0}hr.ttf".format(MEDIA_TMP), 100)
+    # ttfont = ImageFont.truetype("hr.ttf", 20)
+    # ttfont1 = ImageFont.truetype("hr.ttf", 100)
+    draw.text((234, 200), nick, font=ttfont, fill=(0, 0, 0, 255))
+    draw.text((240, 381), day, font=ttfont1, fill=(255, 160, 29, 255))
     # draw.text((187, 592), nick, font=ttfont)
+    # save_path = '{0}.jpg'.format(openid)
     save_path = '{0}{1}.jpg'.format(MEDIA_TMP, openid)
     final1 = final1.convert('RGB')
     final1.save(save_path, quality=100)
@@ -82,7 +96,8 @@ def gen_qh_pic_and_send(nick, avatar, qr_url, openid, token, appid, secret, num)
     logging.info('Start gen qh pic')
     MEDIA_TMP = './static/tmp/'
     # region = Image.open(cStringIO.StringIO(urllib.urlopen(qr_url).read()))
-    base_img = Image.open('{0}baseq1.png'.format(MEDIA_TMP))
+    # base_img = Image.open('{0}baseq1.png'.format(MEDIA_TMP))
+    base_img = Image.open('baseq1.png')
     box = (150, 586, 240, 676)
     ava_box = (28, 113, 128, 213)
     # box = (180, 632, 250, 702)
@@ -104,23 +119,26 @@ def gen_qh_pic_and_send(nick, avatar, qr_url, openid, token, appid, secret, num)
     final1.paste(base_img, (0, 0), base_img)
     final1.paste(output, ava_box, output)
     draw = ImageDraw.Draw(final1)
-    ttfont = ImageFont.truetype("{0}fzpc.ttf".format(MEDIA_TMP), 20)
+    ttfont = ImageFont.truetype("fzpc.ttf", 20)
+    # ttfont = ImageFont.truetype("{0}fzpc.ttf".format(MEDIA_TMP), 20)
     draw.text((85, 225), nick, font=ttfont)
-    draw.text((85, 252), num, font=ttfont)
+    draw.text((85, 252), '1', font=ttfont)
     # draw.text((187, 592), nick, font=ttfont)
-    save_path = '{0}{1}.jpg'.format(MEDIA_TMP, openid)
+    # save_path = '{0}{1}.jpg'.format(MEDIA_TMP, openid)
+    save_path = '{0}.jpg'.format(openid)
     final1 = final1.convert('RGB')
     final1.save(save_path, quality=100)
-    mid = upload_picture('http://sy.chafanbao.com/static/tmp/{0}.jpg'.format(openid), token, appid, secret)
-    wechat = WechatBasic(token, appid, secret)
-    token = wechat.grant_token()['access_token']
-    req_url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={0}'.format(token)
-    # message = message.decode('utf-8')
-    data = {'touser': openid,
-            'msgtype': 'image',
-            'image': {'media_id': mid}}
-    result = requests.post(req_url, data=json.dumps(data))
-    return json.loads(result.content)
+    # mid = upload_picture('http://sy.chafanbao.com/static/tmp/{0}.jpg'.format(openid), token, appid, secret)
+    # wechat = WechatBasic(token, appid, secret)
+    # token = wechat.grant_token()['access_token']
+    # req_url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={0}'.format(token)
+    # # message = message.decode('utf-8')
+    # data = {'touser': openid,
+    #         'msgtype': 'image',
+    #         'image': {'media_id': mid}}
+    # result = requests.post(req_url, data=json.dumps(data))
+    # return json.loads(result.content)
+
 
 @app.task
 def send_pic(openid, token, appid, secret):
@@ -135,3 +153,8 @@ def send_pic(openid, token, appid, secret):
     result = requests.post(req_url, data=json.dumps(data))
     return json.loads(result.content)
 
+
+if __name__ == '__main__':
+    gen_pic_and_send('rapospectre',
+                     'http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKMdylic8UUD4zsQU4Bz58HoLLMmD7z1YfFQ4HyyoIDmhzktSZTSzG3vkcVsfLHFWXy3b4XeAppPTw/132',
+                     '', 'xxx', 'xxx', 'xxx', '', 1)
--- weichat/urls.py
+++ weichat/urls.py
@@ -3,7 +3,7 @@ from LeadYouFly.settings import UPLOAD_DIR
 from weichat.views import *
 
 urlpatterns = patterns('',
-                       url(r'^qh/$', qh_service),
+                       # url(r'^qh/$', qh_service),
                        url(r'^$', wechat_service),
 
 )
